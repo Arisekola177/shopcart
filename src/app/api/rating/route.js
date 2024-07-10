bff2164637c728fba173c01cf1207105b75cec13
Arisekola <shuaibazeez14@gmail.com>
2024-07-11 00:08:42 +0100

    rating

--- src/app/api/rating/route.js
+++ src/app/api/rating/route.js
@@ -3,44 +3,28 @@ import prisma from '../../../libs/prisma';
 import { getUser } from '../../../../actions/getUser';
 
 export async function POST(req) {
-    try {
+
         const currentUser = await getUser();
 
         if (!currentUser) {
-            console.error("User not authenticated");
-            return NextResponse.json({ error: 'User not authenticated' }, { status: 401 });
+            return NextResponse.error();
         }
 
         const body = await req.json();
-        const { comment, rating, productId } = body;
+        const { comment, rating, product, userId } = body;
 
-        const product = await prisma.product.findUnique({
-            where: { id: productId },
-            include: { reviews: true }
-        });
-
-        if (!product) {
-            console.error("Product not found");
-            return NextResponse.json({ error: 'Product not found' }, { status: 404 });
-        }
 
         const deliveredOrder = currentUser?.orders.some(order =>
-            order.products.some(item => item.id === productId) && order.deliveredStatus === 'delivered'
+            order.products.some(item => item.id === product.id) && order.deliveredStatus === 'delivered'
         );
 
-        const userReview = product.reviews.find(review => review.userId === currentUser.id);
+        const userReview = product?.reviews.find(review => review.userId === currentUser.id);
 
-        if (userReview) {
-            console.error("User has already reviewed this product");
-            return NextResponse.json({ error: 'User has already reviewed this product' }, { status: 400 });
+        if(userReview || !deliveredOrder){
+            return NextResponse.error()
         }
 
-        if (!deliveredOrder) {
-            console.error("Product not delivered to user");
-            return NextResponse.json({ error: 'Product not delivered to user' }, { status: 400 });
-        }
-
-        const review = await prisma.review.create({
+        const review = await prisma?.review.create({
             data: {
                 comment,
                 rating,
@@ -50,8 +34,5 @@ export async function POST(req) {
         });
 
         return NextResponse.json(review);
-    } catch (error) {
-        console.error("Error creating review:", error);
-        return NextResponse.json({ error: 'Internal Server Error', details: error.message }, { status: 500 });
-    }
-}
+    } 
+     
